a755156203b41be5c3f792198de3503fdfa833bd
Akash09 <akashbag12a05@gmail.com>
2024-06-22 23:18:43 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="w text-9xl">hello world</h1>
+    </>
+  );
+}
+
+export default App;
+
+// set font poppins
+// set font inter
