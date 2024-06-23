fdeec44b38cfb988405149bf930a4c4636e8bf76
rahul-p19 <rahulpandey.dps@gmail.com>
2024-06-23 22:20:34 +0530

    added Speakers Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import "./App.css";
-import Speakers from "./components/Speakers";
 
 function App() {
   return (
     <>
-      {/* <h1 className="w text-9xl">hello world</h1> */}
-      <Speakers />
+      <h1 className="w text-9xl">hello world</h1>
     </>
   );
 }
