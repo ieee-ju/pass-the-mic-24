05dcc217cf955bbf2a085bfca137167860363b41
Abhishek-465 <abhishekbhattacharjee12345@gmail.com>
2024-06-23 12:12:35 +0530

    added env

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,20 +1,16 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+
+import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDWGjcZzP1NjosvtMNSpF4TI55NhJxK0Nw",
-  authDomain: "pass-the-mic-24.firebaseapp.com",
-  projectId: "pass-the-mic-24",
-  storageBucket: "pass-the-mic-24.appspot.com",
-  messagingSenderId: "552545363053",
-  appId: "1:552545363053:web:296a4abf7a4dc1dadd4b64"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
