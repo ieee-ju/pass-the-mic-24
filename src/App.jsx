faef467a44c9bcaf5cd607127252baf98d5bb3b5
Abhirup154 <pingking29705@gmail.com>
2024-07-03 11:39:22 +0530

    episode commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import About from "./components/About";
 import Contact from "./components/Contact";
-import Loading from './components/Landing'
+import Loading from './components/Landing';
+import Episode from './components/Episode';
 
 function App() {
   return (
